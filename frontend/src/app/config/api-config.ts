23a777d70b7172d8e67a26cb3356fbb6c5b1d367
Dostoyevsky1 <arellano_04_26@hotmail.com>
2026-07-16 23:52:26 -0500

    Actualización render

--- frontend/src/app/config/api-config.ts
+++ frontend/src/app/config/api-config.ts
@@ -3,4 +3,4 @@ import { isDevMode } from '@angular/core';
 
 export const API_BASE_URL = isDevMode() 
   ? 'http://localhost:8080' 
-  : 'https://sistema-tambo.onrender.com'; // Modificable por el dominio final asignado en el despliegue.
+  : 'https://sistema-tambo-backend-rx0c.onrender.com'; // Modificable por el dominio final asignado en el despliegue.
